2ab096876e49c0acecb8f069a6b14d504fdcf2a8
AVioli <68108339+Aadithya-19@users.noreply.github.com>
2021-03-28 14:53:41 +0530

    Add files via upload

--- RemoveFiles.py
+++ RemoveFiles.py
@@ -1,26 +1,80 @@
 import os
 import shutil
-
-path = '/Dummyfolder/'
-days = 30
-
-seconds = (time.time() - days*24*60*60)
+import time
 
 def ageOfFolder(path):
-    
-    ctime = os.stat(path).st_ctime
 
-    return ctime
+	ctime = os.stat(path).st_ctime
 
-if os.path.exists(path):
+	return ctime
+
+
+def removeFolder(path):
+
+	if not shutil.rmtree(path):
+
+		print(f"{path} is removed successfully")
+
+	else:
+
+		print(f"Unable to delete the "+path)
+
+
+
+def removeFile(path):
+	if not os.remove(path):
+
+		print(f"{path} is removed successfully")
+
+	else:
+
+		print("Unable to delete the "+path)
+
+def main():
+
+
+	path = '/Dummyfolder'
+
+	days = 30
+
+
+	seconds = time.time() - (days * 24 * 60 * 60)
+
+	if os.path.exists(path):
+
+		for root_folder, folders, files in os.walk(path):
+
+			if seconds >= ageOfFolder(root_folder):
+
+				removeFolder(root_folder)
+
+				break
+
+			else:
+
+				for folder in folders:
+
+					folder_path = os.path.join(root_folder, folder)
+
+					if seconds >= ageOfFolder(folder_path):
+
+						removeFolder(folder_path)
+
+				for file in files:
+
+					file_path = os.path.join(root_folder, file)
+
+					if seconds >= ageOfFolder(file_path):
+
+						removeFile(file_path)
     
-    for folders, files, root_folder in os.walk(path):
-        for folder in folders:
-            name, ext = os.path.join(root_folder, folder)
-            if seconds >= ageOfFolder(ext):
-                os.remove(ext)
-        
-         for file in files:
-            name, ext = os.path.join(file)
-            if seconds >= ageOfFolder(ext):
-                os.remove(ext)
+		else:
+            
+			if seconds >= ageOfFolder(path):
+
+				removeFile(path)
+	else:
+		print(f"{path} is not found")
+
+if __name__ == '__main__':
+	main()
